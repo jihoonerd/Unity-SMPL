de3b387ad41c3203353f803c64d34a8feab96585
Jihoon Kim <jihoonerd@outlook.com>
2022-07-04 18:48:24 +0900

    Add constructor of motion controller

--- Assets/Scripts/MotionController.cs
+++ Assets/Scripts/MotionController.cs
@@ -5,16 +5,31 @@ using Newtonsoft.Json;
 public class MotionController : MonoBehaviour
 {
     public TextAsset jsonFile;
+    int motionLength;
     MotionData motionData;
 
 
     void Start()
     {
-        motionData = ReadFromJSON(jsonFile.text);
+        motionData = ReadFromJSON(jsonFile.text); // Read the JSON file
+        motionLength = motionData.translation.Length;
+        
         Debug.Log(motionData.label);
         Debug.Log(motionData.translation[0][0]);
+
+        // StartCoroutine(RunMotion());
     }
 
+    // IEnumerator RunMotion()
+    // {
+    //     // yield return new WaitForSeconds(1.0f);
+    //     while(dataReader.GetCurIndex() < dataReader.numFiles)
+    //     {
+    //         yield return new WaitForSeconds(frameInterval);
+    //         MotionData motionData = dataReader.GetMotionData();
+    //         SetPose(motionData, baseRig);
+    //     }
+    // }
 
     MotionData ReadFromJSON(string jsonContents)
     {   
